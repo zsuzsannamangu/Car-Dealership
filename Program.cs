5f712139690e1fd96c6e97758268725824370de2
Student <example@example.com>
2019-04-22 10:42:42 -0700

    add messages to display with each car

--- Program.cs
+++ Program.cs
@@ -7,10 +7,10 @@ namespace Dealership {
   {
     public static void Main()
     {
-      Car porsche = new Car("2014 Porsche 911", 114991, 7864, "");
-      Car ford = new Car("2011 Ford F450", 55995, 14241, "");
-      Car lexus = new Car("2013 Lexus RX 350", 44700, 20000, "");
-      Car mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "");
+      Car porsche = new Car("2014 Porsche 911", 114991, 7864, "Great");
+      Car ford = new Car("2011 Ford F450", 55995, 14241, "Wonderful");
+      Car lexus = new Car("2013 Lexus RX 350", 44700, 20000, "Excellent choice");
+      Car mercedes = new Car("Mercedes Benz CLS550", 39900, 37979, "Perfect");
 
       List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes };
 
@@ -45,6 +45,7 @@ namespace Dealership {
         Console.WriteLine(automobile.GetMakeModel());
         Console.WriteLine(automobile.GetMiles() + " miles");
         Console.WriteLine("$" + automobile.GetPrice());
+        Console.WriteLine(automobile.GetMessage());
       }
     }
   }
